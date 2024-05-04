05968920dbf30a6a20208d80b1176abfbe9ec414
nnasko <nnasskooo@gmail.com>
2024-05-04 09:53:23 +0100

    styled some things

--- app/components/PostsForm.tsx
+++ app/components/PostsForm.tsx
@@ -4,6 +4,8 @@ import { useForm } from 'react-hook-form';
 import { createPost } from '../../lib/actions/authActions'; 
 import { useSession } from 'next-auth/react';
 import { toast } from 'react-toastify';
+import { Button, Textarea } from '@nextui-org/react';
+import { Input } from 'postcss';
 
 const PostsForm = () => {
     const { data: session, status  } = useSession({ 
@@ -33,18 +35,15 @@ const PostsForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="title">Title:</label>
-      <input id="title" {...register('title', { required: true })} />
-      {errors.title && <p className="error-message">Title is required.</p>} 
+    <form onSubmit={handleSubmit(onSubmit)} className='m-6 bg-foreground border-2 p-4 h-min rounded-lg'>
+      <div className='dark'>
+      <Textarea label="Description" id="description" {...register('description', { required: true })} />
+      {errors.description && <p className="text-sm p-2">Description is required.</p>} 
 
-      <label htmlFor="description">Description:</label>
-      <textarea id="description" {...register('description', { required: true })} />
-      {errors.description && <p className="error-message">Description is required.</p>} 
-
-      <button type="submit" disabled={submitting}>
+      <Button type="submit" disabled={submitting}>
         {submitting ? 'Submitting...' : 'Create Post'}
-      </button>
+      </Button>
+      </div>
     </form>
   );
 };
